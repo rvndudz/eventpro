be0d5f6487a535b11e259671c9da629a06ba70d9
rvndudz <rvndudz77@gmail.com>
2024-10-19 13:26:21 +0530

    add main page

---
+++ components/shared/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <Button asChild className="rounded-full" size="lg">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
