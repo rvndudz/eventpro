ecf995ae5d78a5fd887e750314daa2eb00cd97da
rvndudz <rvndudz77@gmail.com>
2024-10-20 13:29:11 +0530

    add hero

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 EventPro. All Rights reserved.</p>
+        <p>2024 EventPro. All Rights reserved.</p>
       </div>
     </footer>
   );
