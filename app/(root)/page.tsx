b54912aba0653c39eb4f112c4e2716f916413601
rvndudz <rvndudz77@gmail.com>
2024-11-15 21:09:23 +0530

    update homepage text for clarity and engagement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,11 +25,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Host, Connect, Celebrate: Your Events, Our Platform!
+              Create, Connect, and Inspire: Your Event Journey Starts Here!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
-              Book and learn helpful tips from 3,168+ mentors in world-class
-              companies with our global community.
+              Join a vast network of professionals and learn from mentors at world-class companies. Discover insights, connect with experts, and elevate your events with our global community.
             </p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">Explore Now</Link>
@@ -51,7 +50,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         className="wrapper my-8 flex flex-col gap-8 md:gap-12"
       >
         <h2 className="h2-bold">
-          Trust by <br /> Thousands of Events
+          Explore <br /> Thousands of Events
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
