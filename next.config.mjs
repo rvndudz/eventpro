60fbc8a42b8801b58dff3cb86b7535e6bbcefefe
rvndudz <rvndudz77@gmail.com>
2025-01-11 13:19:15 +0530

    feat: integrate Stripe for checkout process and add webhook for order creation

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    domains: ["utfs.io", "yf4pquysoq.ufs.sh"], 
     remotePatterns: [
       {
         protocol: "https",
         hostname: "utfs.io",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "yf4pquysoq.ufs.sh",
+        port: "",
+      },
     ],
   },
 };
+
 export default nextConfig;
